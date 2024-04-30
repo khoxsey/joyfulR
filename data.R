6010619df0c0c089b36d8562298846c1ad3ff6f3
khoxsey <khoxsey@gmail.com>
2024-04-30 11:42:46 -0700

    Initial setup

---
+++ data.R
@@ -0,0 +1,8 @@
+# S&P 500 Index data broadly available
+# Rstudio/gt here: https://github.com/rstudio/gt/tree/master/data
+#
+# The ever-reliable Yahoo Finance has everything since the GFC:
+require(quantmod)
+getSymbols('^SPX')
+getSymbols('SPY')
+
